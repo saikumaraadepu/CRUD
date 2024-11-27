071c84307283425e93c4e4e377920f0837528b4e
Saikumar Adepu <saikumaraadepu1@gmail.com>
2024-11-27 11:17:20 +0530

    Added Logging

--- src/main/java/com/crud/crudapp/Product/commandhandlers/CreateProductCommandHandler.java
+++ src/main/java/com/crud/crudapp/Product/commandhandlers/CreateProductCommandHandler.java
@@ -5,6 +5,8 @@ import com.crud.crudapp.Exceptions.ProductNotValidException;
 import com.crud.crudapp.Product.Model.Product;
 import com.crud.crudapp.Product.ProductRepository;
 import io.micrometer.common.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +18,12 @@ public class CreateProductCommandHandler implements Command<Product, Product> {
 		this.productRepository = productRepository;
 	}
 
+	private static final Logger logger = LoggerFactory.getLogger(CreateProductCommandHandler.class);
+
 	@Override
 	public ResponseEntity<Product> execute(Product product) {
+
+		logger.info("Executing {} with {}", getClass(), product.toString());
 
 		validateProduct(product);
 
@@ -27,18 +33,22 @@ public class CreateProductCommandHandler implements Command<Product, Product> {
 	private void validateProduct(Product product) {
 
 		if (StringUtils.isBlank(product.getName())){
+			logger.error("Product name is empty");
 			throw new ProductNotValidException("Product name cannot be empty");
 		}
 
 		if (StringUtils.isBlank(product.getDescription())){
+			logger.error("Product description is empty");
 			throw new ProductNotValidException("Product description cannot be empty");
 		}
 
 		if (product.getPrice() <= 0.0) {
+			logger.error("Product price is invalid");
 			throw new ProductNotValidException("Product price cannot be negative");
 		}
 
 		if (product.getQuantity() <= 0) {
+			logger.error("Product quantity is invalid");
 			throw new ProductNotValidException("Product quantity cannot be negative");
 		}
 	}
